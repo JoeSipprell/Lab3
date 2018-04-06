58327e2546a639ce20590d961e297bfab667ffc1
JoeSipprell <kensipprell@gmail.com>
2018-04-06 13:30:49 -0500

    finished lab - made some minor improvements to the user interface - added more annotation to the code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,57 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean quitProgram = false;
 
-        //passwordEntry(input);
-        //wordCount(input);
-        //phoneNumberGen(input);
-        idGen(input);
+        while (true) {
+            System.out.println("\nEnter the corresponding number for the program you would like to run\n");
+            System.out.println("1. Set Password\n2. Word Count\n3. Phone Number Formatter\n4. ID Generator\n5. Quit");
+
+            String programNum = input.nextLine();
+
+            switch (programNum) {
+
+                case "1":
+                    passwordEntry(input);
+                    break;
+
+                case "2":
+                    wordCount(input);
+                    break;
+
+                case "3":
+                    phoneNumberGen(input);
+                    break;
+
+                case "4":
+                    idGen(input);
+                    break;
+
+                case "5":
+                    quitProgram = true;
+                    break;
+
+                default:
+                    System.out.println("\n***ERROR: INVALID INPUT***");
+                    System.out.println("Example: to run the Set Password program, enter just the digit '1'");
+                    System.out.println("Press enter to try again");
+                    input.nextLine();
+                    break;
+            }
+
+            if (quitProgram) {
+                System.out.println("\nProgram closed.");
+                break;
+            }
+        }
 
     }
 
+    // passwordEntry determines if user-inputted string contains between 6 and 10 characters, 1 number, 1 lowercase letter, and 1 uppercase letter.
+    // is run from main method
     private static void passwordEntry(Scanner input)
     {
+        System.out.println("\nPassword Entry Program");
         System.out.println("Please enter a password that is 6-10 characters long and contains 1 number, 1 letter, and 1 uppercase");
         String password;
         Boolean hasNum = false, hasLet = false, hasUpp = false;
@@ -53,8 +94,11 @@ public class Main {
 
     } // end passwordEntry
 
+    // wordCount counts the number of words in a sentence gotten from user input.
+    // Any chunk of text that has whitespace on both sides is counted as a word.
+    // run from main method
     private static void wordCount(Scanner input){
-
+        System.out.println("\nWord Count Program");
         System.out.println("Please enter a sentence"); // input of sentence
         String sentence = input.nextLine();
 
@@ -73,7 +117,10 @@ public class Main {
         System.out.println(String.format("There were %d words in the sentence.",numWords));
     } // end of wordCount
 
+    // phoneNumberGen formats a 10 digit number (stored as a string, gotten from user input) like a phone number.
+    // is run from main method
     private static void phoneNumberGen(Scanner input){
+        System.out.println("\nPhone Number Formatting Program");
         System.out.println("Please enter a 10 digit phone number, or enter '999' to quit.");
 
         String numEntered = "";
@@ -105,12 +152,15 @@ public class Main {
         }
     } // end of phoneNumberGen
 
+    // idGen generates a user ID, starting with the user's full initials, followed by the users address number.
+    // If user has a hyphenated name, both names are included in initials
+    // is run from main method
     private static void idGen(Scanner input){
-        System.out.println("ID Generator");
+        System.out.println("\nID Generator Program");
 
 
         while (true) {
-            System.out.print("Please enter your full name (enter '999' to quit): ");
+            System.out.println("Please enter your full name (enter '999' to quit): ");
             String nameIn = input.nextLine().trim();
 
             if(nameIn.equals("999")) { break; }
@@ -135,8 +185,11 @@ public class Main {
             System.out.println("Press enter to create another ID");
             input.nextLine();
         }
-    }
+    } // end idGen
 
+    // createID returns a string that will contain the first letter of each word in fullName, followed by any numbers detected in address (if no errors occur)
+    // returned string will contain error messages if any occur
+    // used by idGen
     public static String createID(String fullName, String address)
     {
         String outString = "";
@@ -168,10 +221,11 @@ public class Main {
             outString += "nodigits";
         }
 
-
         return outString;
-    }
+    } // end createID
 
+    // checkIfNumeric returns a boolean value based on if the character is a number or not
+    //      Is used by createID
     public static Boolean checkIfNumeric(char digit){
         boolean isNumeric;
 
@@ -184,5 +238,5 @@ public class Main {
         }
 
         return isNumeric;
-    }
+    } // end checkIfNumeric
 }
